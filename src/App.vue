f0fd07cd472317adff1ca2c633ebdd8b561bbacd
alexisForman <alexisgforman@gmail.com>
2023-05-03 16:41:44 -0500

    classwork trying to learn vue - todo task

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ToDoItem />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from './components/ToDoItem.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ToDoItem,
   }
 }
 </script>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="ToDos">
+      <h1>Lets make a list of everything you need to do today!</h1>
+      <ul>
+        <li v-for="item in items" v-bind:key="item">
+            <label for="checkbox">{{ item }}</label>
+            <input type="checkbox">
+        </li>
+
+        <input type="text" v-model="item" placeholder="Add your new to do item here...">
+        <button @click="addItem">Add new item here</button>
+      </ul>
+      
+      
+    </div>
+</template>
+  
+<script>
+  export default {
+    name: 'ToDoItem',
+    props: {
+      
+    },
+    data()  {
+        return  {
+            items: [
+                "vaccuum the floors",
+                "dust everything",
+                "clean out the fridge"
+            ],
+            item: ""
+        }
+    },
+    methods:    {
+
+
+        addItem()  {
+            this.items.push(this.item);
+        }
+    }
+
+  }
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  </style>
+  
